953fcca9fd5eed96e7144eb7c7336f71f7aa1749
Winston Saunders <winstonasaunders@gmail.com>
2014-12-08 21:13:21 -0800

    updated

--- TwitterReader.R
+++ TwitterReader.R
@@ -37,7 +37,7 @@ user <- getUser(paste0("@", name))
 ## n is an adjustable variable
 ## user_data is a generic filename
 
-user_data<-userTimeline(user, n=1500, maxID=NULL, sinceID=NULL, includeRts=FALSE)
+user_data<-userTimeline(user, n=2000, maxID=NULL, sinceID=NULL, includeRts=FALSE)
 
 
 ##convert data to dataframe
@@ -50,7 +50,7 @@ clean_data<-do.call(rbind,lapply(user_data,as.data.frame))
 write.csv(clean_data, paste0(name,"_N",".csv"))
 
 ##display a bunch of stuff
-print(paste0(name,".csv"))
+print(paste0(name,"_N",".csv"))
 dim(clean_data)
 head(clean_data)
 
